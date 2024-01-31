50c640b5684e21e7e34b3b1b3e51e8f79a10332b
skysoon02 <skysoon02@kaist.ac.kr>
2024-01-31 14:11:04 +0900

    delete DB contents from config

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-import pymysql
-from sqlalchemy import create_engine
-from sshtunnel import SSHTunnelForwarder
-
 '''
 References
 https://github.com/yt-dlp/yt-dlp#embedding-yt-dlp
@@ -13,17 +9,4 @@ startDate = '20240123'          #값 변경 뒤에는 path_videoList 폴더를 �
 periodDownloadComments = 3
 
 number_of_process = 16
-chunk_size = 100
 
-## DB Setting
-DB_ENV = {
-    'SSH_HOST': '143.248.249.232',
-    'SSH_PORT': 3221,
-    'SSH_USER': 'youtube',
-    'SSH_PASSWORD': 'youtube',
-    'HOST': 'localhost',
-    'PORT': 3306,
-    'USER': 'root',
-    'PASSWORD': '',
-    'DATABASE': 'YOUTUBE_2024'
-}
