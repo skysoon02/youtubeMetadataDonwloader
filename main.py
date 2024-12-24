717587406aee20e798aa42170f4daa47a51766c5
skysoon02 <skysoon02@kaist.ac.kr>
2024-12-24 19:13:13 +0900

    download only once

--- main.py
+++ main.py
@@ -93,6 +93,55 @@ def main():
         print('Successfully donwloaded all video metadatas\n')
 
 
+def main_onlyOnce():
+    init()
+    getCommentsOpt = True
+
+    ### (1) Download new video URLs of channels
+    print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]'), end=' ')
+    print('Start downloading video URLs\n')
+    
+    with Pool(number_of_process) as pool:
+        pool.map(worker_VideoListDownloader, channels)
+        
+    print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]'), end=' ')
+    print('Successfully downloaded all video URLs\n')
+
+    ### (2) Download videos' metadatas and thumbnails
+    print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]'), end=' ')
+    print('Start downloading video metadatas\n')
+
+    que = Queue()
+    toDownloadVideoURLs = []    #여러 *.txt 파일의 URL 전부 읽기
+    for channel in channels:
+        path = path_videoList + '/videoList_' + channel['name'] + '.jsonl'
+        with jsonlines.open(path, 'r') as f:
+            for data in f.iter():
+                toDownloadVideoURLs.append(data['webpage_url'])
+    
+    print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]'), end=' ')
+    print('The number of entire URLs: {0}'.format(len(toDownloadVideoURLs)))
+            
+    que = Queue()
+    for url in toDownloadVideoURLs:
+        que.put(url)
+    
+    processes=[]
+    for i in range(number_of_process):
+        videoDownloader = VideoDownloader(getCommentsOpt)            
+        process = Process(target=videoDownloader.download, args=(que, ))
+        processes.append(process)
+        process.start()
+
+    for process in processes:
+        process.join()
+    
+    print(datetime.now().strftime('[%Y-%m-%d %H:%M:%S]'), end=' ')
+    print('Successfully donwloaded all video metadatas\n')
+
+
+
+
 if __name__ == '__main__':
     #debug()
     main()
